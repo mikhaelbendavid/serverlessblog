d3a790543b2d7a1cd9b1fd944c762af8182d2bcd
Mikhael ben David <litchevm@gmail.com>
2017-07-22 13:24:10 -0700

    Created component item to highlight what page our browser is on

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Routes from './Routes';
-import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+import { Link, withRouter } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+import RouteNavItem from './components/RouteNavItem';
 import './App.css';
 
 class App extends Component {
+  handleNavLink = (event) => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute('href'));
+  }
   render() {
     return (
-      <div classname="App container">
+      <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
@@ -15,6 +20,12 @@ class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <RouteNavItem onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>
+              <RouteNavItem onClick={this.handleNavLink} href="/login">Login</RouteNavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
@@ -22,4 +33,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
