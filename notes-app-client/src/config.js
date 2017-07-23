850a225610e0c5fa3a9400e4dd8adaf49facca4b
Mikhael ben David <litchevm@gmail.com>
2017-07-23 12:35:33 -0700

    Troubleshooting Login issue with config variables not being defined

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID : "us-east-2_t6tTEb8QD",
+    APP_CLIENT_ID : "52vsuvcodusl9lmeqcr4maq4ck"
+  }
+};
--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import '../config.js';
+import { CognitoUserPool, AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js';
 import './Login.css';
+
 
 class Login extends Component {
   constructor(props) {
@@ -22,9 +25,38 @@ class Login extends Component {
   });
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const userToken = await this.login(this.state.username, this.state.password);
+      alert(userToken);
+    }
+    catch(e) {
+      alert(e);
+    }
   }
+
+  login(username, password) {
+  const userPool = new CognitoUserPool({
+    UserPoolId: config.cognito.USER_POOL_ID,
+    ClientId: config.cognito.APP_CLIENT_ID
+  });
+  const authenticationData = {
+    Username: username,
+    Password: password
+  };
+
+  const user = new CognitoUser({ Username: username, Pool: userPool });
+  const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+  return new Promise((resolve, reject) => (
+    user.authenticateUser(authenticationDetails, {
+      onSuccess: (result) => resolve(result.getIdToken().getJwtToken()),
+      onFailure: (err) => reject(err),
+    })
+  ));
+}
 
   render() {
     return (
